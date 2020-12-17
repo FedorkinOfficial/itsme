9772914aba311ebb7a16938a47268fd58313f7e4
fedor!@gmail.com <fedor!@gmail.com>
2020-12-17 15:29:16 +0200

    Add URL to HEADERS vercel to index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $method = $_SERVER['REQUEST_METHOD'];
-header('Access-Control-Allow-Origin: http://itsmeapi');
+header('Access-Control-Allow-Origin: https://itsme-ten.vercel.app');
 header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Credentials: true');
