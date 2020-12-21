d3065220c8ae507b30a2db1ed2185cd36ad634c0
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 13:07:55 +0200

    Change with credentials

--- src/components/Header/Header.Conatainer.jsx
+++ src/components/Header/Header.Conatainer.jsx
@@ -9,14 +9,12 @@ import Cookie from 'universal-cookie';
 class HeaderAPI extends React.Component{
     componentDidMount(){
         // let authLogin = (data) => this.props.authLoginAC(data);
-        // fetch("https://mineproapi.000webhostapp.com/api/users").then(function(response) {
-        //         console.log(response);
-        //         return response.json();
-        //     });
-        //     // then(function(body) {
-        //     //     // authLogin(response.data.info);
-        //     //     console.log(body) 
-        //     // });.
+        fetch("https://mineproapi.000webhostapp.com/api/rusers",{method: 'POST', credentials: 'include'}).then(function(response) {
+                    console.log(response);
+                    return response.json();
+                }).then(function(body) {
+                    console.log(body);
+                });
         // axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}, {withCredentials: true}).then(function(response) {
         //         console.log(response);
         //         if(!response.data.status){
@@ -27,19 +25,19 @@ class HeaderAPI extends React.Component{
         //             console.log(response.data)
         //         }
         //     })
-        let cookie = new Cookie();
-        let formData = new FormData();
-        formData.append('token', cookie.get('token'));
-        axios({
-            method: 'post',
-            url: 'https://mineproapi.000webhostapp.com/api/rusers',
-            data: formData,
-            withCredentials: true
-            }, {withCredentials: true})
-            .then(function (response) {
-                console.log(response);
-                console.log(response.data);
-            });
+        // let cookie = new Cookie();
+        // let formData = new FormData();
+        // formData.append('token', cookie.get('token'));
+        // axios({
+        //     method: 'post',
+        //     url: 'https://mineproapi.000webhostapp.com/api/rusers',
+        //     data: formData,
+        //     withCredentials: true
+        //     }, {withCredentials: true})
+        //     .then(function (response) {
+        //         console.log(response);
+        //         console.log(response.data);
+        //     });
     }
  
     render(){
