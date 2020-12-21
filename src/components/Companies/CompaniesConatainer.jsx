8ccbd6663d68102246e34a76ab03ac0e82fea10c
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 14:42:58 +0200

    Change with credentials

--- src/components/Companies/CompaniesConatainer.jsx
+++ src/components/Companies/CompaniesConatainer.jsx
@@ -7,20 +7,39 @@ import axios from 'axios';
 class ComapniesAPI extends React.Component{
     componentDidMount(){
         let findComapny = (data) => this.props.findComapnyAC(data)
-        axios.get('https://itsmeapi/api/companies', {withCredentials: true}).then(function(response) {
-                console.log(response);
+        let cookie = new Cookie();
+        let formData = new FormData();
+        formData.append('token', cookie.get('token'));
+        axios({
+            method: 'post',
+            url: 'https://mineproapi.000webhostapp.com/api/companies',
+            data: formData,
+            })
+            .then(function (response) {
                 if(!response.data.status){
-                     console.log("There are not companies");
+                    console.log("There are not companies");
                 } else {
                     console.log(response.data);
                     findComapny(response.data);
-                }
-            })
+                }             
+            });
+        
+        // axios.get('https://itsmeapi/api/companies', {withCredentials: true}).then(function(response) {
+        //         console.log(response);
+        //         if(!response.data.status){
+        //              console.log("There are not companies");
+        //         } else {
+        //             console.log(response.data);
+        //             findComapny(response.data);
+        //         }
+        //     })
     }
     addCompany = () => {
         let formData = new FormData();
+        let cookie = new Cookie();
         formData.append('name', this.props.state.companies.changedName);
         formData.append('info', this.props.state.companies.changedInfo);
+        formData.append('token', cookie.get('token'));
         fetch(`https://itsmeapi/api/company`, {method: 'POST', body: formData}, {withCredentials: true}).then(function(response) {
                 console.log(response);
                 return response.json();
