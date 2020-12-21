eb314f90ff96501cc6ae9ef196ea1c1ec22aedc9
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 11:42:55 +0200

    Change with credentials

--- src/components/Header/Header.Conatainer.jsx
+++ src/components/Header/Header.Conatainer.jsx
@@ -17,7 +17,7 @@ class HeaderAPI extends React.Component{
         //     //     // authLogin(response.data.info);
         //     //     console.log(body) 
         //     // });.
-        axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}).then(function(response) {
+        axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}, {withCredentials: true}).then(function(response) {
                 console.log(response);
                 if(!response.data.status){
                      console.log("There are not cookies");
