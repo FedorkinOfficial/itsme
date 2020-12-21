106c462ffa161e3e571b59b6eaea372711f48e16
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 11:55:15 +0200

    Change with credentials

--- src/components/Header/Header.Conatainer.jsx
+++ src/components/Header/Header.Conatainer.jsx
@@ -17,16 +17,16 @@ class HeaderAPI extends React.Component{
         //     //     // authLogin(response.data.info);
         //     //     console.log(body) 
         //     // });.
-        axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}, {withCredentials: true}).then(function(response) {
-                console.log(response);
-                if(!response.data.status){
-                     console.log("There are not cookies");
-                     console.log(response.data);
-                } else {
-                    // authLogin(response.data.info);
-                    console.log(response.data)
-                }
-            })
+        // axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}, {withCredentials: true}).then(function(response) {
+        //         console.log(response);
+        //         if(!response.data.status){
+        //              console.log("There are not cookies");
+        //              console.log(response.data);
+        //         } else {
+        //             // authLogin(response.data.info);
+        //             console.log(response.data)
+        //         }
+        //     })
         let cook = Cookies.get('token');
         let formData = new FormData();
         formData.append('token', cook);
