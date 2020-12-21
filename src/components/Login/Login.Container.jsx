9679806afe5ec762cd19d7acea310e6c0f792b3e
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 09:24:05 +0200

    Change with credentials

--- src/components/Login/Login.Container.jsx
+++ src/components/Login/Login.Container.jsx
@@ -9,7 +9,7 @@ class LoginAPI extends React.Component{
         let formData = new FormData();
         formData.append('name', this.props.state.login.changedName);
         formData.append('pass', this.props.state.login.changedPass);
-        await fetch(`https://mineproapi.000webhostapp.com/api/login`, {method: 'POST', body: formData, mode: 'cors', credentials: 'same-origin'}).then(function(response) {
+        fetch(`https://mineproapi.000webhostapp.com/api/login`, {method: 'POST', body: formData, mode: 'cors', credentials: 'same-origin'}).then(function(response) {
                 console.log(response);
                 return response.json();
             }).then(function(body) {
