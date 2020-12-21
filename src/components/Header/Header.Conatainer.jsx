f6946ec2ab2b83d6ee5bc400524f21316bea617a
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 09:40:25 +0200

    Change with credentials

--- src/components/Header/Header.Conatainer.jsx
+++ src/components/Header/Header.Conatainer.jsx
@@ -9,23 +9,23 @@ import {authLoginAC} from '../../reducers/loginReducer';
 class HeaderAPI extends React.Component{
     componentDidMount(){
         // let authLogin = (data) => this.props.authLoginAC(data)
-        fetch("https://mineproapi.000webhostapp.com/api/users").then(function(response) {
-                console.log(response);
-                return response.json();
-            });
-            // then(function(body) {
-            //     // authLogin(response.data.info);
-            //     console.log(body) 
-            // });.
-        // axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}).then(function(response) {
+        // fetch("https://mineproapi.000webhostapp.com/api/users").then(function(response) {
         //         console.log(response);
-        //         if(!response.data.status){
-        //              console.log("There are not cookies");
-        //         } else {
-        //             authLogin(response.data.info);
-        //             console.log(response.data)
-        //         }
-        //     })
+        //         return response.json();
+        //     });
+        //     // then(function(body) {
+        //     //     // authLogin(response.data.info);
+        //     //     console.log(body) 
+        //     // });.
+        axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}).then(function(response) {
+                console.log(response);
+                if(!response.data.status){
+                     console.log("There are not cookies");
+                } else {
+                    authLogin(response.data.info);
+                    console.log(response.data)
+                }
+            })
     }
  
     render(){
