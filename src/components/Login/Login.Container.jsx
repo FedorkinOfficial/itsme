74fad82ba7c987267e6e3d4a306ce688b58b44f9
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 08:52:20 +0200

    Change with credentials

--- src/components/Login/Login.Container.jsx
+++ src/components/Login/Login.Container.jsx
@@ -10,7 +10,6 @@ class LoginAPI extends React.Component{
         formData.append('name', this.props.state.login.changedName);
         formData.append('pass', this.props.state.login.changedPass);
         fetch(`https://mineproapi.000webhostapp.com/api/login`, {method: 'POST', body: formData, credentials: "include"}).then(function(response) {
-                console.log(response.headers['set-cookie']);
                 console.log(response);
                 return response.json();
             }).then(function(body) {
