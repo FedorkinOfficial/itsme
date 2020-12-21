74040b24a750c483a83d4410197868062c666786
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 13:31:50 +0200

    Change with credentials

--- src/components/Header/Header.Conatainer.jsx
+++ src/components/Header/Header.Conatainer.jsx
@@ -30,7 +30,7 @@ class HeaderAPI extends React.Component{
         formData.append('token', cookie.get('token'));
         axios({
             method: 'post',
-            url: 'http://itsmeapi/api/rusers',
+            url: 'https://itsmeapi/api/rusers',
             data: formData,
             })
             .then(function (response) {
