410755f2646f4ba09df8828c933b764c2e980bdc
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 09:38:54 +0200

    Change with credentials

--- src/components/Header/Header.Conatainer.jsx
+++ src/components/Header/Header.Conatainer.jsx
@@ -8,14 +8,15 @@ import {authLoginAC} from '../../reducers/loginReducer';
 
 class HeaderAPI extends React.Component{
     componentDidMount(){
-        let authLogin = (data) => this.props.authLoginAC(data)
-        fetch("https://mineproapi.000webhostapp.com/api/users", {credentials: 'include'}).then(function(response) {
+        // let authLogin = (data) => this.props.authLoginAC(data)
+        fetch("https://mineproapi.000webhostapp.com/api/users").then(function(response) {
                 console.log(response);
                 return response.json();
-            }).then(function(body) {
-                // authLogin(response.data.info);
-                console.log(body) 
             });
+            // then(function(body) {
+            //     // authLogin(response.data.info);
+            //     console.log(body) 
+            // });.
         // axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}).then(function(response) {
         //         console.log(response);
         //         if(!response.data.status){
