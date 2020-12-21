751b77cffbb55dfd5c524bbeac51f73aa5587d98
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 09:28:50 +0200

    Change with credentials

--- src/components/Header/Header.Conatainer.jsx
+++ src/components/Header/Header.Conatainer.jsx
@@ -9,15 +9,22 @@ import {authLoginAC} from '../../reducers/loginReducer';
 class HeaderAPI extends React.Component{
     componentDidMount(){
         let authLogin = (data) => this.props.authLoginAC(data)
-        axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}).then(function(response) {
+        fetch(`https://mineproapi.000webhostapp.com/api/users`, {method: 'GET', mode: 'cors', credentials: 'include'}).then(function(response) {
                 console.log(response);
-                if(!response.data.status){
-                     console.log("There are not cookies");
-                } else {
-                    authLogin(response.data.info);
-                    console.log(response.data)
-                }
-            })
+                return response.json();
+            }).then(function(body) {
+                // authLogin(response.data.info);
+                console.log(response.body) 
+            });
+        // axios.get('https://mineproapi.000webhostapp.com/api/users', {withCredentials: true}).then(function(response) {
+        //         console.log(response);
+        //         if(!response.data.status){
+        //              console.log("There are not cookies");
+        //         } else {
+        //             authLogin(response.data.info);
+        //             console.log(response.data)
+        //         }
+        //     })
     }
  
     render(){
