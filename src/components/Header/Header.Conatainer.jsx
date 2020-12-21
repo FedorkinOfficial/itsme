16f81bf58aba82bd159caf62860ca7673b430686
fedor!@gmail.com <fedor!@gmail.com>
2020-12-21 11:57:17 +0200

    Change with credentials

--- src/components/Header/Header.Conatainer.jsx
+++ src/components/Header/Header.Conatainer.jsx
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import Header from './Header';
 import axios from 'axios';
 import {authLoginAC} from '../../reducers/loginReducer';
-import { Cookies } from 'react-cookie'
+// import { Cookies } from 'react-cookie';
 
 
 class HeaderAPI extends React.Component{
