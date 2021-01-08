255f7ca4c70a741154d71dda714fba618401b576
fedor!@gmail.com <fedor!@gmail.com>
2021-01-08 20:41:26 +0200

    Change IP

--- src/components/Register/Register.Container.jsx
+++ src/components/Register/Register.Container.jsx
@@ -8,7 +8,7 @@ class RegisterAPI extends React.Component{
         let formData = new FormData();
         formData.append('name', this.props.state.register.changedName);
         formData.append('pass', this.props.state.register.changedPass);
-        fetch(`https://95.47.116.121/api/users`, {mode: 'no-cors', method: 'POST', body: formData, credentials: "include"}).then(function(response) {
+        fetch(`https://95.47.116.121/users`, {mode: 'no-cors', method: 'POST', body: formData, credentials: "include"}).then(function(response) {
                 console.log(response);
                 return response.json();
             }).then(function(body) { 
